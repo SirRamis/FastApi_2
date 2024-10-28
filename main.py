7ef18b6300706c7e09efc1495437b4a6b2cf00f3
Ramis <sir.ramis@mail.ru>
2024-10-28 13:31:38 +0300

    все работает

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-from typing import Optional
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+# def fake_answer_to_everything_ml_model(x: float):
+#     return x * 42
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_tables()
+    print("База готова к работе")
+    yield
+    print("Выключение")
 
 
 app = FastAPI(
+    lifespan=lifespan,
     title="Martin Pozishn"
 )
-
-
-class STaskAdd(BaseModel):
-    name: str
-    description: Optional[str] = None
-
-class STask(STaskAdd):
-    id: int
-
-
-
-tasks = []
-@app.post("/tasks")
-async def add_task(
-    task: STaskAdd,
-):
-    tasks.append(task)
-    return {"ok": True}
-
-
+app.include_router(tasks_router)
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
